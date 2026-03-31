e14ff93196941ce91670d0f10ada292b70e0415e
unknown <andylee0311a@google.com>
2026-03-31 17:03:19 +0800

    修復 package.json 格式問題

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // 加入這一行，讓路徑變成相對路徑！
+})
