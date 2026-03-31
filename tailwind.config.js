6605e58e5e5064b18d7dd17319ffe104b4a36b9d
unknown <andylee0311a@google.com>
2026-03-31 17:14:09 +0800

    修復 Tailwind 設定檔位置與內容

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // 👈 這行很重要，代表要掃描 src 裡面的檔案
   ],
   theme: {
     extend: {},
