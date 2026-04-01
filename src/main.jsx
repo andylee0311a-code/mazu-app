4490e106a32dc87fe2faba4f932b3de11bbbdd8f
unknown <andylee0311a@google.com>
2026-04-01 09:25:30 +0800

    補回 main.jsx 的 React 渲染指令

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // 👈 確保有這一行！
+import './index.css'
+
+// 👇 就是少了下面這段「把畫面畫出來」的指令！
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
